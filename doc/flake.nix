4b22df07e5e23ff1cbd4c8a00631678c4b443d2d
connerohnesorge <conneroisu@outlook.com>
2025-05-18 16:51:59 -0500

    latest

---
+++ doc/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "bufrnix documentation";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    bun2nix.url = "github:baileyluTCD/bun2nix";
+    bun2nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {nixpkgs,bun2nix, ...}: let
+    systems = [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          bun
+          bun2nix.packages.${system}.default
+        ];
+      };
+    });
+  };
+}
