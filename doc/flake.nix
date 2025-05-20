4f10d237925faf3faa108c710c9d15bfcd74c892
connerohnesorge <conneroisu@outlook.com>
2025-05-20 11:12:58 -0500

    remove treefmt from doc shell/flake

--- doc/flake.nix
+++ doc/flake.nix
@@ -5,13 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     bun2nix.url = "github:baileyluTCD/bun2nix";
     bun2nix.inputs.nixpkgs.follows = "nixpkgs";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs = {
     nixpkgs,
     bun2nix,
-    treefmt-nix,
     ...
   }: let
     systems = [
@@ -22,24 +20,7 @@
     ];
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
-
-    # Evaluate the treefmt modules from ./treefmt.nix
-    treefmtEval = forAllSystems (
-      system:
-        treefmt-nix.lib.evalModule
-        nixpkgs.legacyPackages.${system}
-        ./treefmt.nix
-    );
   in {
-    formatter = forAllSystems (
-      system:
-        treefmtEval.${system}.config.build.wrapper
-    );
-
-    checks = forAllSystems (system: {
-      formatting = treefmtEval.${system}.config.build.check ./.;
-    });
-
     devShells = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
